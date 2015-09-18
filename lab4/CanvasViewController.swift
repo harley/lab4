9b751477ee809555b9184db35ca70487eaa54e35
minhchau273 <minhchau273@gmail.com>
2015-09-18 10:58:20 +0700

    Create new face

--- lab4/CanvasViewController.swift
+++ lab4/CanvasViewController.swift
@@ -16,23 +16,18 @@ class CanvasViewController: UIViewController {
     var trayOriginalCenter: CGPoint!
     var downY: CGFloat!
     var upY: CGFloat!
-    var animator: UIDynamicAnimator!
+    
+    var initialNewFaceCenter: CGPoint!
+    
+    @IBOutlet weak var happyView: UIImageView!
+    
+    var newlyCreatedFace: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.animator = UIDynamicAnimator(referenceView: view)
-        
-        var gravityBehavior = UIGravityBehavior(items: [trayView])
-        animator.addBehavior(gravityBehavior)
-        
-        var collisionBehavior = UICollisionBehavior(items: [trayView])
-        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
-        animator.addBehavior(collisionBehavior)
 
         downY = 635
         upY   = 460
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -59,14 +54,7 @@ class CanvasViewController: UIViewController {
             if velocity.y > 0 {
                 // close the tray
                 trayView.center = CGPoint(x: trayOriginalCenter.x, y: downY)
-                
             } else {
-//                self.trayView.center = CGPoint(x: self.trayOriginalCenter.x, y: self.upY)
-//                var elasticityBehavior = UIDynamicItemBehavior(items: [trayView])
-//                elasticityBehavior.elasticity = 0.7
-//                animator.addBehavior(elasticityBehavior)
-
-
                 UIView.animateWithDuration(2, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 2, options: nil, animations: { () -> Void in
                     self.trayView.center = CGPoint(x: self.trayOriginalCenter.x, y: self.upY)
                 }, completion: { (bool) -> Void in
@@ -80,6 +68,49 @@ class CanvasViewController: UIViewController {
             
         }
     }
+    
+    
+    @IBAction func onFacePan(sender: UIPanGestureRecognizer) {
+        
+        var state = sender.state
+        var translation = sender.translationInView(self.view)
+        
+        var faceView = sender.view as! UIImageView
+        
+        newlyCreatedFace = UIImageView(image: faceView.image)
+        newlyCreatedFace.userInteractionEnabled = true
+        
+        if state == UIGestureRecognizerState.Began {
+            
+            
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = faceView.center
+            
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+//            newlyCreatedFace.center.y += 100
+            
+            initialNewFaceCenter = newlyCreatedFace.center
+            
+            println("new x: \(newlyCreatedFace.center.x)")
+            println("new y: \(newlyCreatedFace.center.y)")
+        }
+        
+        if state == UIGestureRecognizerState.Changed {
+            println("before new x: \(newlyCreatedFace.center.x)")
+            println("before new y: \(newlyCreatedFace.center.y)")
+            
+            newlyCreatedFace.center.x = initialNewFaceCenter.x + translation.x
+            newlyCreatedFace.center.y = initialNewFaceCenter.y + translation.y
+            
+            initialNewFaceCenter = newlyCreatedFace.center
+            println("after new x: \(newlyCreatedFace.center.x)")
+            println("after new y: \(newlyCreatedFace.center.y)")
+            
+            sender.setTranslation(CGPointZero, inView: self.view)
+        }
+        
+    }
+    
     
     
     
